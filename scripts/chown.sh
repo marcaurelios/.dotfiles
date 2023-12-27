1ccdefa60f003bb442610f17caad6ad73a7b2993
Aurelios <write.aurel@gmail.com>
2023-12-27 23:31:59 +0100

    Update chown.sh

--- scripts/chown.sh
+++ scripts/chown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /tmp/downloads /tmp/incomplete
-chown -R 1001:1001 /tmp/downloads /tmp/incomplete
+mkdir -p /tmp/downloads /tmp/incomplete /tmp/yt
+chown -R 1001:1001 /tmp/downloads /tmp/incomplete /tmp/yt
 
 exit 0
