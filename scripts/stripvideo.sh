452f9046e713d45e3bf7a93a75be871ee8ce3421
marcaurelios <write.aurel@gmail.com>
2023-12-27 23:22:17 +0100

    removes dupe script

--- scripts/stripvideo.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh 
-
-duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$1")
-newDuration=$(python3 -c "print($duration - ($3 + $4))")
-
-ffmpeg -i "$1" -ss $3 -t $newDuration -c:v 'libx265' -q:v 60 
