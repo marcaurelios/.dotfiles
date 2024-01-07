c5bc52f45a640c85ca1d42f8dc5bae62649ded22
Aurelios <write.aurel@gmail.com>
2024-01-07 12:25:51 +0100

    updates configs

--- scripts/chown.sh
+++ scripts/chown.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-mkdir -p /var/tmp/qbit
-chown -R 1000:1000 /var/tmp/qbit
-chmod -R 775 /var/tmp/qbit
-=======
-mkdir -p /tmp/downloads /tmp/incomplete /tmp/yt
-chown -R 1001:1001 /tmp/downloads /tmp/incomplete /tmp/yt
->>>>>>> 1ccdefa60f003bb442610f17caad6ad73a7b2993
+mkdir -p /var/tmp/qbit /var/tmp/qbit_private
+chown -R 1000:1000 /var/tmp/qbit /var/tmp/qbit_private
+chmod -R 775 /var/tmp/qbit /var/tmp/qbit_private
 
 exit 0
